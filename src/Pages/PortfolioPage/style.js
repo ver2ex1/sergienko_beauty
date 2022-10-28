f86bbd71ddc644bb29803afa2f918ca9af557301
ver2ex1 <hlushkovadym212@gmail.com>
2022-10-28 17:41:05 +0300

    refactoring

--- src/Pages/PortfolioPage/style.js
+++ src/Pages/PortfolioPage/style.js
@@ -8,18 +8,13 @@ const getStyles = () => {
       [theme.breakpoints.down(426)]: {
         paddingTop: "25%",
       },
-      [theme.breakpoints.down(376)]: {
-        paddingTop: "30%",
-      },
-      [theme.breakpoints.down(321)]: {
-        paddingTop: "50%",
-      },
+
       "& .slick-slider": {
         margin: "auto",
         maxWidth: "90%",
         [theme.breakpoints.down(769)]: {
           maxWidth: "80%",
-          paddingTop: "2%",
+          paddingTop: "5%",
         },
       },
       "& img": {
